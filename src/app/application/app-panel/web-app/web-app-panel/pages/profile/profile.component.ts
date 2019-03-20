84aae08265bb7d00e67b29012fc52930ea591a53
Aditya Suman <sumanaditya19@gmail.com>
2019-03-20 10:25:49 +0530

    updated projects section

--- src/app/application/app-panel/web-app/web-app-panel/pages/profile/profile.component.ts
+++ src/app/application/app-panel/web-app/web-app-panel/pages/profile/profile.component.ts
@@ -9,8 +9,15 @@ export class ProfileComponent implements OnInit {
 
   private repeat = [1,2,3];
   private post = [1,2,2,2,2,2,2,2,2,2,2,2];
-  private skills = ['Java', 'C++', 'C', 'PHP', 'SQL', 'JavaScript', 'Node Js', 'HTML 5', 'CSS', 'jQuery', 'noSQL', 'Mongo DB', 'React', 'Python', 'Angular', 'RxJs', 'Redux'];
   
+  private skill = [
+    {section: 'Top Skills', skills: ['Web Development', 'Automotive', 'Electronics']},
+    {section: 'Tools and Technology', skills: ['Java', 'C++', 'C', 'PHP', 'SQL', 'JavaScript', 'Node Js', 'HTML 5', 'CSS', 'jQuery', 'noSQL', 'Mongo DB', 'React', 'Python', 'Angular', 'RxJs', 'Redux']},
+    {section: 'Other Skills', skills: ['Team Work', 'Leadership', 'Startups', 'Networking', 'Business Development']}
+  ]
+
+
+
   constructor() { }
 
   ngOnInit() {
